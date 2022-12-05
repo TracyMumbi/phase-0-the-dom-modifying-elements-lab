6c1465a1a8dfbddb4f0572ef2062b4925fc41c4d
Tracy Mumbi <tracy.mumbi@student.moringaschool.com>
2022-12-05 15:23:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main. remove();
+
+
+const newHeader = document.createElement("h1");
+// document.body.append(newHeader);
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'TRACY is the champion';
+// document.body.append(newHeader);
+
